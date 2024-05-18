fdef36f88581a8077f4870e518d75a73535a0050
Mir Mansoor <mirmansoor100@gmail.com>
2024-05-18 14:08:43 +0530

    Serve static Views

--- pyJs/pyJs/urls.py
+++ pyJs/pyJs/urls.py
@@ -21,4 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('service/', views.service, name='service')
 ]
--- pyJs/pyJs/views.py
+++ pyJs/pyJs/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "website/index.html")
+
+def about(request):
+    return render(request, "website/about.html")
+
+def contact(request):
+    return render(request, "website/contact.html")
+
+def service(request):
+    return render(request, "website/service.html")
